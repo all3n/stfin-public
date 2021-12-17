18ca12016502ad071140c84573f9d408a1a604a6
wanghuacheng <wanghch8398@163.com>
2021-12-17 20:28:15 +0800

    update

--- mock/table.js
+++ mock/table.js
@@ -13,7 +13,7 @@ const data = Mock.mock({
 
 module.exports = [
   {
-    url: '/api/table/list',
+    url: '/table/list',
     type: 'get',
     response: config => {
       const items = data.items
